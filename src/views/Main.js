ac4e2c07ab23918a7d953ea875a2e8fd7a967689
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2023-03-10 11:32:48 +0530

    Modify the structure of the home page

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,23 +1,24 @@
 import React from 'react';
+import "./App.css";
 import { Box } from '@mui/system';
 import { Image, Heading } from '@chakra-ui/react';
 import WithSubnavigation from '../components/Navbar';
-
+import HomePage from './HomePage';
 
 function Main() {
   return (
-    <div id = "main">
-      <div style={{position: 'relative'}}>
-        <Image src='https://img3.goodfon.com/original/1920x1080/e/83/nastolnyy-tennis-ping-pong.jpg' alt='Dan Abramov'></Image>
-        <div style={{position: 'absolute', top: 0, left: 0, width: '100%', textAlign: 'center'}}>
-          <Heading as='h1' size='4xl' padding = '370px' noOfLines={1} color='white'>
-            MORA SMASHES - 2023
-          </Heading>
-        </div>
-      </div>
 
+      <div className="App"><HomePage /></div>
 
-    </div>
+      // <div style={{position: 'relative'}}>
+      //   {/* <Image src='https://img3.goodfon.com/original/1920x1080/e/83/nastolnyy-tennis-ping-pong.jpg' alt='Dan Abramov'></Image> */}
+      //   <div style={{position: 'absolute', top: 0, left: 0, width: '100%', textAlign: 'center'}}>
+      //     <Heading as='h1' size='4xl' padding = '370px' noOfLines={1} color='white'>
+      //       MORA SMASHES - 2023
+      //     </Heading>
+      //   </div>
+      // </div>
+
 
   );
 }
