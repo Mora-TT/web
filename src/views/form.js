c2fab7736a58afaa962c0b83cc0bfa0e3ba83880
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2023-03-14 20:52:15 +0530

    Change the events list

--- src/views/form.js
+++ src/views/form.js
@@ -6,9 +6,10 @@ import {
   FormErrorMessage,
   FormHelperText,
   Link,
-  Button
+  Button,
+  Select
 } from '@chakra-ui/react';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Input, Heading } from '@chakra-ui/react'
 import { Checkbox, CheckboxGroup } from '@chakra-ui/react'
 import WithSubnavigation from '../components/Navbar';
@@ -44,6 +45,127 @@ function Forms() {
   //   console.log(file);
   //   // You can perform any operation on the uploaded file here
   // });
+//   <FormControl paddingBottom={5} >
+//   <Checkbox >Novices - Under 19 Singles</Checkbox>
+// </FormControl>
+// <FormControl paddingBottom={5} >
+//   <Checkbox >Novices - Under 17 Singles</Checkbox>
+// </FormControl>
+// <FormControl paddingBottom={5} >
+//   <Checkbox >Novices - Under 15 Singles</Checkbox>
+// </FormControl>
+// <FormControl paddingBottom={5} >
+//   <Checkbox >Novices - Under 13 Singles</Checkbox>
+// </FormControl>
+// <FormControl paddingBottom={5} >
+//   <Checkbox >Novices - Under 11 Singles</Checkbox>
+// </FormControl>
+// <FormControl paddingBottom={5} >
+//   <Checkbox >Novices - Under 09 Singles</Checkbox>
+// </FormControl>
+// <FormControl paddingBottom={5} >
+//   <Checkbox >Novices - Under 07 Singles</Checkbox>
+// </FormControl>
+
+  const [totalCount, setTotalCount] = useState(0);
+  const [eventsList, setEventList] = useState([]);
+
+  function ChangeCount(count) {
+    console.log(count);
+    // console.log(eventsList);
+    if ((count.checked) && (eventsList.includes(count.name) == false)) {
+      eventsList.push(count.name);
+      setEventList(eventsList)
+      let newCount = eventsList.length * 100;
+      setTotalCount(newCount);
+      console.log(newCount);
+    } else if((eventsList.includes(count.name) == true)) {
+      eventsList.splice(eventsList.indexOf(count.name))
+      let newCount = eventsList.length * 100;
+      setTotalCount(newCount);
+      setEventList(eventsList)
+    }
+    useEffect(() => {
+      // This function will be called whenever the `value` state changes
+      // and will update the value display accordingly.
+      console.log(`Value changed to ${totalCount}`);
+    }, [totalCount]);
+  }
+
+  const NovicesEvents = () => {
+    const options = [
+      {
+        value: 'under19',
+        label: 'Novices - Under 19 Singles',
+        price: 100
+      },
+      {
+        value: 'under17',
+        label: 'Novices - Under 17 Singles',
+        price: 100
+      },
+      {
+        value: 'under15',
+        label: 'Novices - Under 15 Singles',
+        price: 100
+      },
+      {
+        value: 'under13',
+        label: 'Novices - Under 13 Singles',
+        price: 100
+      },
+      {
+        value: 'under11',
+        label: 'Novices - Under 11 Singles',
+        price: 100
+      },
+      {
+        value: 'under9',
+        label: 'Novices - Under 09 Singles',
+        price: 100
+      },
+      {
+        value: 'under7',
+        label: 'Novices - Under 07 Singles',
+        price: 100
+      },
+    ]
+    return (
+      <div >
+      {options.map(o => (
+        <FormControl paddingBottom={5} key={o.value} name={o.value} >
+          <Checkbox  key={o.value} id = {o.value} name={o.value} > {o.label} </Checkbox>         
+          {/* <input type="checkbox" key={o.value} id = {o.value} name={o.value} onChange={e => ChangeCount(e.target)} /> */}
+        </FormControl>
+      ))}
+      </div>
+    );
+  }
+  const Dropdown = ({}) => {
+    const options = [
+      {
+        name: 'Male',
+        value: 'Male',
+        label: 'Male'
+      },
+      {
+        name: 'Female',
+        value: 'Female',
+        label: 'Female'
+      }
+    ]
+    const [selectedOption, setSelectedOption] = useState(options[0].value);
+    return (
+      <Select variant='outline' defaultValue={" "}
+      onChange={e => console.log(e.target.value)}>
+        <option  value={" "} disabled></option>
+      {options.map(o => (
+        <option key={o.value} value={o.value}>{o.label}</option>
+      ))}
+       
+      </Select>
+    );
+  };
 
   return (
     <div>
@@ -108,6 +230,11 @@ function Forms() {
           <Input type="text" id="ttasl-reg-no" name="ttasl-reg-no" />
         </FormControl>
 
+        <FormControl isRequired paddingBottom={5}>
+          <FormLabel>Gender</FormLabel>
+          <Dropdown />
+        </FormControl>
+
         <FormControl isRequired paddingBottom={5} >
           <FormLabel>Payment Slip</FormLabel>
           <input id="file-upload" type="file" name="file-upload-input" />
@@ -118,20 +245,14 @@ function Forms() {
         </Heading>
 
         <FormControl  paddingBottom={5}>
-          <Checkbox >Intermediate Men's Singles</Checkbox>
+          <Checkbox >Intermediate Singles</Checkbox>
         </FormControl>
         <FormControl  paddingBottom={5}>
-          <Checkbox >Intermediate Women's Singles</Checkbox>
-        </FormControl>
-        <FormControl  paddingBottom={5}>
-          <Checkbox >Novices Men's Singles</Checkbox>
-        </FormControl>
-        <FormControl  paddingBottom={5}>
-          <Checkbox >Novices Women's Singles</Checkbox>
+          <Checkbox >Novices Singles</Checkbox>
         </FormControl>
 
-        <Heading as='h4' size='1xl' paddingBottom={5} paddingTop={5} noOfLines={1} >
-          AGE GROUP
+        <Heading as='h4' size='0.25xl' paddingBottom={5} paddingTop={5} noOfLines={1} >
+          * Age Group
         </Heading>
 
         <FormControl isRequired paddingBottom={5}>
@@ -154,46 +275,13 @@ function Forms() {
           <Input type="number" id="age-days" name="age-days"  />
         </FormControl>
 
-        <FormControl paddingBottom={5} >
-          <Checkbox >Novices Girl's under 19 singles</Checkbox>
-        </FormControl>
-        <FormControl paddingBottom={5} >
-          <Checkbox >Novices Boy's under 17 singles</Checkbox>
-        </FormControl>
-        <FormControl paddingBottom={5} >
-          <Checkbox >Novices Girl's under 17 singles</Checkbox>
-        </FormControl>
-        <FormControl paddingBottom={5} >
-          <Checkbox >Novices Boy's under 15 singles</Checkbox>
-        </FormControl>
-        <FormControl paddingBottom={5} >
-          <Checkbox >Novices Girl's under 15 singles</Checkbox>
-        </FormControl>
-        <FormControl paddingBottom={5} >
-          <Checkbox >Novices Boy's under 13 singles</Checkbox>
-        </FormControl>
-        <FormControl paddingBottom={5} >
-          <Checkbox >Novices Girl's under 13 singles</Checkbox>
-        </FormControl>
-        <FormControl paddingBottom={5} >
-          <Checkbox >Novices Boy's under 11 singles</Checkbox>
-        </FormControl>
-        <FormControl paddingBottom={5} >
-          <Checkbox >Novices Girl's under 11 singles</Checkbox>
-        </FormControl>
-        <FormControl paddingBottom={5} >
-          <Checkbox >Novices Boy's under 09 singles</Checkbox>
-        </FormControl>
-        <FormControl paddingBottom={5} >
-          <Checkbox >Novices Girl's under 09 singles</Checkbox>
-        </FormControl>
-        <FormControl paddingBottom={5} >
-          <Checkbox >Novices Boy's under 07 singles</Checkbox>
-        </FormControl>
-        <FormControl paddingBottom={5} >
-          <Checkbox >Novices Girl's under 07 singles</Checkbox>
-        </FormControl>
+        <Heading as='h4' size='0.25xl' paddingBottom={5} paddingTop={5} noOfLines={1} >
+          * Novices Events
+        </Heading>
 
+        <NovicesEvents />
+
+        <p> Total Price - {totalCount}</p>
         <Button type="submit" marginTop={5} marginLeft={30} marginBottom={100} style={{justifyContent: 'center'}}>Submit</Button>
         </form>
       </Box>
